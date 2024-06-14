d771ec4191be888c5c66c709dfaeb14593af0655
IreneKanorina82 <happywoman08@mail.ru>
2024-06-15 00:45:46 +0300

    Class

--- test_belavia.py
+++ test_belavia.py
@@ -2,10 +2,10 @@
 import allure
 import pytest_check as check
 import time
-from telnetlib import EC
 import unittest
 from pages.base_page import MainPage
 from pages.base_page import WebPage
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from termcolor import colored
@@ -14,7 +14,7 @@ from termcolor import colored
 
 class WebElement(object):
 
-    _locator = ('MainPage', WebPage'https://belavia.by/')
+    _locator = ('MainPage', WebPage 'https://belavia.by/')
     _web_driver = None
     _page = None
     _timeout = 10
@@ -44,19 +44,19 @@ class WebElement(object):
     def is_clickable(self):
         """ Check is element ready for click or not. """
 
-        element = self.wait_to_be_clickable(timeout=0.1)
+        element = self.wait_to_be_clickable(timeout=1)
         return element is not None
 
     def is_presented(self):
         """ Check that element is presented on the page. """
 
-        element = self.find(timeout=0.1)
+        element = self.find(timeout=1)
         return element is not None
 
     def is_visible(self):
         """ Check is the element visible or not. """
 
-        element = self.find(timeout=0.1)
+        element = self.find(timeout=1)
 
         if element:
             return element.is_displayed()
@@ -65,7 +65,7 @@ class WebElement(object):
 
 
 @allure.story('Тест для проверки главной страницы')
-@allure.feature('Тест для проверки хедара')
+@allure.feature('Тест для проверки хедера')
 def test_headers(web_browser):
     """Этот тест проверяет хедер главной страницы"""
 
@@ -76,7 +76,7 @@ def test_headers(web_browser):
     elements_headers = [
         (page.btn_headers_menu, 'Меню'),
         (page.btn_headers_RU, 'Русский'),
-        (page.btn_headers_ffp-account, 'Личный кабинет'),
+        (page.btn_headers_ffpAccount, 'Личный кабинет'),
         (page.btn_headers_bad_vision, 'Обычная версия')
     ]
 
@@ -145,18 +145,18 @@ def test_input_main(web_browser):
         check.is_true(page.input_main_wrapper.is_clickable())
 
     with allure.step('Проверка плейсхолдера'):
-        check.equal(page.input_main_wrapper.get_attribute('placeholder'), 'Введите домены')
+        check.equal(page.input_main_wrapper.get_attribute('placeholder'), 'Откуда')
 
     with allure.step('Проверка на ввод текст и проверка результата'):
         text_by_zone = page.text_by_zone.get_text()
-        text_tatimati = 'tatimati'
+        text_cockicock = 'cockicock'
 
-        page.input_main_wrapper.send_keys(text_tatimati)
+        page.input_main_wrapper.send_keys(text_cockicock)
         page.btn_search_domain.click(1)
 
         time.sleep(10)
         print(page.group_valid_domain.get_text())
-        check.not_equal(page.group_valid_domain.get_text().find(f'{text_tatimati+text_by_zone}'), -1)
+        check.not_equal(page.group_valid_domain.get_text().find(f'{text_cockicock+text_by_zone}'), -1)
 
 
 @allure.story('Тест для проверки главной страницы')
@@ -175,7 +175,7 @@ def test_link_main(web_browser):
 
 
 @allure.story('Тест для проверки главной страницы')
-@allure.feature('Тест для проверки колличества плиток в блоке "Подберите свое решение"')
+@allure.feature('Тест для проверки количества плиток в блоке "Подберите свое решение"')
 def test_count_box_main(web_browser):
     """Этот тест проверяет количество плиток в блоке "Подберите свое решение" """
 
@@ -185,7 +185,7 @@ def test_count_box_main(web_browser):
 
     with allure.step('Проверка количества плиток'):
         count = page.btn_count_box
-        number = [(4), (9), (10), (3), (1), (4), (11), (3), (5), (5), (53)]
+        number = [(4), (9), (10), (3), (1), (4), (11), (3), (5), (53)]
 
         for count_elements in count:
             count_elements.click()
@@ -231,9 +231,9 @@ wait = WebDriverWait(driver, 10)
 wait.until(EC.presence_of_element_located((By.ID, "doubleClickBtn")))
 
 # Нахождение элементов на странице
-single_click_btn = driver.find_element_by_id("singleClickBtn")
-double_click_btn = driver.find_element_by_id("doubleClickBtn")
-right_click_btn = driver.find_element_by_id("rightClickBtn")
+single_click_btn = driver.find_element(By.ID, "singleClickBtn")
+double_click_btn = driver.find_element(By.ID, "doubleClickBtn")
+right_click_btn = driver.find_element(By.ID, "rightClickBtn")
 
 # Нажатие на кнопки
 single_click_btn.click()
