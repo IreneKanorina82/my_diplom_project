bed29b7857de7199707f5b7088f616d5d7ff1975
IreneKanorina82 <happywoman08@mail.ru>
2024-06-26 19:32:11 +0300

    Class

---
+++ scriptsh/run_aut_lk.ps1
@@ -0,0 +1,5 @@
+cd C:\Users\User\PycharmProjects\my_diplom_project
+ . .venv\Scripts\Activate.ps1
+ python -m pytest --alluredir C:\Users\User\PycharmProjects\my_diplom_project\allure_reports
+ pytest -v -s "C:\Users\User\PycharmProjects\my_diplom_project\page_test\tests_belavia"
+
--- scriptsh/runallure.ps1
+++ scriptsh/runallure.ps1
@@ -1,5 +1,1 @@
-cd C:\Users\User\PycharmProjects\my_diplom_project
- . .venv\Scripts\Activate.ps1
- python -m pytest --alluredir C:\Users\User\PycharmProjects\my_diplom_project\allure_reports
- pytest -v -s "C:\Users\User\PycharmProjects\my_diplom_project\page_test\tests_belavia"
  allure serve C:\Users\User\PycharmProjects\my_diplom_project\allure_reports
